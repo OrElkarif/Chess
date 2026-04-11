10dbcf9acc27e32a54cfeb16d00aebe8ef52a280
OrElkarif <orelkarif100@gmail.com>
2026-04-11 19:04:52 +0300

    Delete FinalProject directory

--- FinalProject/src/logic/ActionNode.java
+++
@@ -1,9 +0,0 @@
-package logic;
-
-public abstract class ActionNode extends DecisionNode {
-    public ActionNode(DecisionNode fallbackNode) {
-        super(null, fallbackNode);
-    }
-    @Override
-    public boolean isActionNode() { return true; }
-}
--- FinalProject/src/logic/QuestionNode.java
+++
@@ -1,9 +0,0 @@
-package logic;
-
-public abstract class QuestionNode extends DecisionNode {
-    public QuestionNode(DecisionNode yesNode, DecisionNode noNode) {
-        super(yesNode, noNode);
-    }
-    @Override
-    public boolean isActionNode() { return false; }
-}
