b61275f309d98977e34cd79e0561764e36124f7c
OrElkarif <orelkarif100@gmail.com>
2026-04-29 14:12:55 +0300

    Delete FinalProject directory

--- FinalProject/src/logic/TreeTask.java
+++
@@ -1,7 +0,0 @@
-package logic;
-
-public interface TreeTask {
-    boolean isAction();                    // האם זו פעולה או שאלה
-    boolean checkCondition(GameState state); // מופעל אם זו שאלה
-    Move calculateMove(GameState state);     // מופעל אם זו פעולה
-}
